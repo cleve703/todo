9357a88c2d8d17e675980abf80fa81bbe7341f8c
Jay Conner <cleve703@gmail.com>
2021-05-06 04:47:52 +0000

    add checkbox to tasks and enable add task button and cancel button

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ function createTaskListItems(task) {
   const taskList = document.getElementById('task-ul-' + task.project);
   const taskListItem = document.createElement('li');
   taskListItem.setAttribute('id', 'task-list-item' + task.id);
-  taskListItem.textContent = task.description;
+  taskListItem.textContent = '☐ ' + task.description;
   taskList.appendChild(taskListItem);
 }
 
@@ -64,14 +64,35 @@ function createAddTaskButton(project) {
   const taskUl = document.getElementById('task-ul-' + project.id)
   var addTaskLi = document.createElement('li');
   addTaskLi.setAttribute('class', 'add-task-li');
+  addTaskLi.setAttribute('id', 'add-task-li-' + project.id)
   taskUl.appendChild(addTaskLi);
-  addTaskLi.innerHTML = '<button class="add-task-button"><svg width="13" height="13"><path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z" fill="currentColor" fill-rule="evenodd"></path></svg></button><span class="add-task-label">Add task</span></li>'
+  const addTaskButton = document.createElement('button');
+  addTaskButton.setAttribute('class', 'add-task-button');
+  addTaskButton.setAttribute('id', 'add-task-button-' + project.id);
+  addTaskLi.appendChild(addTaskButton);
+  addTaskButton.innerHTML = '<svg width="13" height="13"><path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z" fill="currentColor" fill-rule="evenodd"></path></svg>'
+  addTaskLabel = document.createElement('span');
+  addTaskLabel.setAttribute('class', 'add-task-label');
+  addTaskLabel.textContent = 'Add task';
+  addTaskLi.appendChild(addTaskLabel)
+  addTaskButton.addEventListener('click', toggleInputFieldOn)
+}
+
+function toggleInputFieldOn() {
+  this.parentElement.parentElement.lastChild.setAttribute('style', 'display: block')
+  this.parentElement.setAttribute('style',  'display: none');
+}
+
+function toggleInputFieldOff() {
+  this.parentElement.parentElement.lastChild.setAttribute('style', 'display: none')
+  this.parentElement.previousSibling.setAttribute('style',  'display: block');
 }
 
 function createInputField(project) {
   const taskUl = document.getElementById('task-ul-' + project.id)
   var addInputLi = document.createElement('li');
   addInputLi.setAttribute('class', 'add-input-li');
+  addInputLi.setAttribute('id', 'add-input-li-project-' + project.id)
   const taskInput = document.createElement('input');
   taskInput.setAttribute('id', 'task-input-to-project-' + project.id)
   const taskSaveButton = document.createElement('button');
@@ -82,6 +103,7 @@ function createInputField(project) {
   taskCancelButton.setAttribute('id', 'cancel-add-to-project-' + project.id);
   taskCancelButton.setAttribute('class', 'cancel-button');
   taskCancelButton.textContent = 'Cancel';
+  taskCancelButton.addEventListener('click', toggleInputFieldOff)
   taskUl.appendChild(addInputLi);
   addInputLi.appendChild(taskInput);
   addInputLi.appendChild(taskSaveButton);
