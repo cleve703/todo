14baefc3ee3d627a0cde9b0ada0d8b19395f0bea
Jay Conner <cleve703@gmail.com>
2021-05-06 02:16:26 +0000

    add factory functions

--- src/index.js
+++ src/index.js
@@ -1,40 +1,96 @@
-// import loadPage from './modules/initial-page-load';
-// import toDoInitialize from './modules/todo-items';
+let projectCounter = 0;
+let taskCounter = 0;
+let allProjects = [];
 
-// init();
+createProject.prototype.f = function() {};
 
-// function init() {
-//   loadPage();
-  
-
-// }
-
-function createCheckBox(id, textSymbol, taskDesc) {
-  const checkBoxDiv = document.createElement('div');
-  checkBoxDiv.setAttribute('id', id)
-  const checkBox = document.createElement('span');
-  checkBox.setAttribute('id', 'checkBoxSpan')
-  checkBox.textContent = textSymbol;
-  checkBoxDiv.appendChild(checkBox);
-  const textLine = document.createElement('span');
-  textLine.setAttribute('id', 'textLine')
-  textLine.textContent = taskDesc;
-  checkBoxDiv.appendChild(textLine);
-  return checkBoxDiv;
+function createProject(description, id=projectCounter) {
+  let project = Object.create(createProject.prototype);
+  project.id = id;
+  project.description = description;
+  project.complete = false;
+  project.tasks = [];
+  projectCounter++;
+  allProjects.push(project);
+  return project;
 }
 
-function toggleCheck(evt) {
-  if (evt.target.innerHTML[0] == '☐') {
-    evt.target.innerHTML = '☑'
-    document.getElementById('textLine').setAttribute('style', 'text-decoration: line-through')
-  }
-  else if (evt.target.innerHTML[0] == '☑') {
-    evt.target.innerHTML = '☐'
-    document.getElementById('textLine').setAttribute('style', 'text-decoration: none')
+createTask.prototype.f = function() {};
+
+function createTask(description, project, id=taskCounter) {
+  let task = Object.create(createTask.prototype)
+  task.description = description;
+  task.id = id;
+  task.complete = false;
+  task.project = project;
+  taskCounter++;
+  allProjects[project].tasks.push(task);
+}
+
+function initProjects() {
+  if (allProjects.length == 0) {
+    createProject('Welcome');
+    createTask('eat', '0');
   }
 }
 
-content = document.getElementById('jumbotron');
-const checkBox = createCheckBox('checkBox', '☐',  'First task');
-content.appendChild(checkBox);
-checkBox.addEventListener('click', toggleCheck);
+function createProjectDiv(desc) {
+  const projectContainer = document.getElementById('jumbotron');
+  const projectDiv = document.createElement('div');
+  projectDiv.setAttribute('id', 'project-' + desc.id + '-div');
+  projectDiv.setAttribute('class', 'project-div');
+  projectDiv.textContent = desc.description;
+  projectContainer.appendChild(projectDiv);
+  const taskUl = document.createElement('ul');
+  taskUl.setAttribute('id', 'task-ul-' + desc.id);
+  taskUl.setAttribute('class', 'task-ul');
+  projectDiv.appendChild(taskUl);
+  if (desc.tasks.length > 0) {
+    desc.tasks.forEach(createTaskListItems)
+  }
+  createAddTaskButton(desc);
+  createInputField(desc);
+}
+
+function createTaskListItems(task) {
+  const taskList = document.getElementById('task-ul-' + task.project);
+  const taskListItem = document.createElement('li');
+  taskListItem.setAttribute('id', 'task-list-item' + task.id);
+  taskListItem.textContent = task.description;
+  taskList.appendChild(taskListItem);
+}
+
+function createAddTaskButton(project) {
+  const taskUl = document.getElementById('task-ul-' + project.id)
+  var addTaskLi = document.createElement('li');
+  addTaskLi.setAttribute('class', 'add-task-li');
+  taskUl.appendChild(addTaskLi);
+  addTaskLi.innerHTML = '<button class="add-task-button"><svg width="13" height="13"><path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z" fill="currentColor" fill-rule="evenodd"></path></svg></button><span class="add-task-label">Add task</span></li>'
+}
+
+function createInputField(project) {
+  const taskUl = document.getElementById('task-ul-' + project.id)
+  var addInputLi = document.createElement('li');
+  addInputLi.setAttribute('class', 'add-input-li');
+  const taskInput = document.createElement('input');
+  taskInput.setAttribute('id', 'task-input-to-project-' + project.id)
+  const taskSaveButton = document.createElement('button');
+  taskSaveButton.setAttribute('id', 'save-to-project-' + project.id);
+  taskSaveButton.setAttribute('class', 'save-button');
+  taskSaveButton.textContent = 'Save';
+  const taskCancelButton = document.createElement('button');
+  taskCancelButton.setAttribute('id', 'cancel-add-to-project-' + project.id);
+  taskCancelButton.setAttribute('class', 'cancel-button');
+  taskCancelButton.textContent = 'Cancel';
+  taskUl.appendChild(addInputLi);
+  addInputLi.appendChild(taskInput);
+  addInputLi.appendChild(taskSaveButton);
+  addInputLi.appendChild(taskCancelButton);
+}
+
+function displayProjects() {
+  allProjects.forEach(createProjectDiv);
+}
+
+initProjects();
+displayProjects();
