a434624281d35b21b9b5be363bc98ebc15c03e43
Jay Conner <cleve703@gmail.com>
2021-05-23 13:32:20 +0000

    add edit and delete buttons to projects - not functional yet

--- src/modules/initial-page-load.js
+++ src/modules/initial-page-load.js
@@ -15,8 +15,15 @@ function buildProjectsHtml () {
     // create project description as first li
     const projectNameLi = createHtmlElement('li', 'project-name', 'project-name-' + e.id);
     projectNameLi.textContent = e.title;
-    projectNameLi.innerHTML += '<hr style="margin-top: 1px; color: lightgray;"></hr>'
     projectDiv.appendChild(projectNameLi);
+    // create edit and delete icons for each project
+    const editButton = createHtmlElement('button', 'edit-button', 'edit-project-button-' + e.id);
+    editButton.innerHTML = '<svg xmlns="http://www.w3.org/20 00/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>';
+    projectNameLi.appendChild(editButton);
+    const deleteButton = createHtmlElement('button', 'delete-button', 'delete-project-button' + e.id);
+    deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';
+    projectNameLi.appendChild(deleteButton);
+    projectNameLi.innerHTML += '<hr style="margin-top: 1px; color: lightgray;"></hr>';
     // create nested ul of tasks for each project
     const projectUl = createHtmlElement('ul', 'project-task-list', 'project-task-list-' + e.id);
     projectDiv.appendChild(projectUl);
